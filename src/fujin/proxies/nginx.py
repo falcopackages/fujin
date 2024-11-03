ff0d1f5b3faa30c188a0c71964291ca9de665dc4
Tobi DEGNON <tobidegnon@proton.me>
2024-11-03 21:59:15 +0100

    implement nginx proxy

--- src/fujin/proxies/nginx.py
+++ src/fujin/proxies/nginx.py
@@ -11,14 +11,12 @@ from fujin.connection import Connection
 CERTBOT_EMAIL = os.getenv("CERTBOT_EMAIL")
 
 
-# TODO: this is a wip
-
-
 class WebProxy(msgspec.Struct):
     conn: Connection
     domain_name: str
     app_name: str
     upstream: str
+    statics: dict[str, str]
     local_config_dir: Path
 
     @property
@@ -33,76 +31,102 @@ class WebProxy(msgspec.Struct):
             upstream=config.webserver.upstream,
             app_name=config.app_name,
             local_config_dir=config.local_config_dir,
+            statics=config.webserver.statics,
         )
 
+    def run_pty(self, *args, **kwargs):
+        return self.conn.run(*args, **kwargs, pty=True)
+
     def install(self):
-        # TODO: won"t always install the latest version, install certbot with uv ?
-        # https://certbot.eff.org/instructions?ws=nginx&os=pip
         self.conn.run(
             "sudo apt install -y nginx libpq-dev python3-dev python3-certbot-nginx"
         )
 
     def uninstall(self):
-        pass
+        self.stop()
+        self.conn.run("sudo apt remove -y nginx")
+        self.conn.run(f"sudo rm /etc/nginx/sites-available/{self.app_name}.conf")
+        self.conn.run(f"sudo rm /etc/nginx/sites-enabled/{self.app_name}.conf")
+        self.conn.run("sudo systemctl disable certbot.timer")
+        self.conn.run("sudo apt remove -y python3-certbot-nginx")
 
     def setup(self):
-        # TODO should not be running all this everytime
         conf = (
             self.config_file.read_text()
             if self.config_file.exists()
             else self._get_config()
         )
-        self.conn.run(
+        self.run_pty(
             f"sudo echo '{conf}' | sudo tee /etc/nginx/sites-available/{self.app_name}.conf",
             hide="out",
-            pty=True,
         )
-        self.conn.run(
+        self.run_pty(
             f"sudo ln -sf /etc/nginx/sites-available/{self.app_name}.conf /etc/nginx/sites-enabled/{self.app_name}.conf",
-            pty=True,
         )
-        self.conn.run("sudo systemctl restart nginx", pty=True)
-        self.conn.run(
-            f"certbot --nginx -d {self.domain_name} --non-interactive --agree-tos --email {CERTBOT_EMAIL} --redirect"
-        )
-        # Updating local Nginx configuration
-        self.conn.get(
-            f"/etc/nginx/sites-available/{self.app_name}.conf",
-            str(self.config_file),
-        )
-        # Enabling certificate auto-renewal
-        self.conn.run("sudo systemctl enable certbot.timer", pty=True)
-        self.conn.run("sudo systemctl start certbot.timer", pty=True)
+        if CERTBOT_EMAIL:
+            cert_path = f"/etc/letsencrypt/live/{self.domain_name}/fullchain.pem"
+            cert_exists = self.conn.run(f"sudo test -f {cert_path}", warn=True).ok
+
+            if not cert_exists:
+                self.conn.run(
+                    f"certbot --nginx -d {self.domain_name} --non-interactive --agree-tos --email {CERTBOT_EMAIL} --redirect"
+                )
+                self.config_file.parent.mkdir(exist_ok=True)
+                self.conn.get(
+                    f"/etc/nginx/sites-available/{self.app_name}.conf",
+                    str(self.config_file),
+                )
+                self.run_pty("sudo systemctl enable certbot.timer")
+                self.run_pty("sudo systemctl start certbot.timer")
+        self.restart()
 
     def teardown(self):
-        pass
+        self.run_pty(f"sudo rm /etc/nginx/sites-available/{self.app_name}.conf")
+        self.run_pty(f"sudo rm /etc/nginx/sites-enabled/{self.app_name}.conf")
+        self.run_pty(
+            "sudo systemctl restart nginx",
+        )
 
-    def start(self) -> None: ...
+    def start(self) -> None:
+        self.run_pty("sudo systemctl start nginx")
 
-    def stop(self) -> None: ...
+    def stop(self) -> None:
+        self.run_pty("sudo systemctl stop nginx")
 
-    def status(self) -> None: ...
+    def status(self) -> None:
+        self.run_pty("sudo systemctl status nginx", warn=True)
 
-    def restart(self) -> None: ...
+    def restart(self) -> None:
+        self.run_pty("sudo systemctl restart nginx")
 
-    def logs(self) -> None: ...
+    def logs(self) -> None:
+        self.run_pty(f"sudo journalctl -u nginx -f", warn=True)
 
     def export_config(self) -> None:
         self.config_file.write_text(self._get_config())
 
     def _get_config(self) -> str:
-        return f"""None
+        static_locations = ""
+        for path, directory in self.statics.items():
+            static_locations += f"""
+        location {path} {{
+            alias {directory};
+        }}
+        """
+
+        return f"""
 server {{
-   listen 80;
-   server_name {self.domain_name};
+    listen 80;
+    server_name {self.domain_name};
 
-   location / {{
-      proxy_pass {self.upstream};
-      proxy_set_header Host $host;
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header X-Forwarded-Proto $scheme;
-   }}
+    {static_locations}
+
+    location / {{
+        proxy_pass {self.upstream};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }}
 }}
-
 """
